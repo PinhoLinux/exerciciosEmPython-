651c93e2f8c24eb8ef198525f3aff203a00e7416
Emerson <147883830+Emerson10110@users.noreply.github.com>
2024-04-20 20:15:33 -0300

    Create Test_medias_alunos.py

---
+++ Test_medias_alunos.py
@@ -0,0 +1,14 @@
+n1 = float(input("Digite a primeira nota")
+n2 = float(input("Digite a segunda nota")
+n3 = float(input("Digite a terceira nota")
+n4r = str(input("na sua escola tem quarta Nota? S/N").upper.strip
+if n4r = S:
+
+  float(input("Digite a quarta nota")
+        media = (n1 + n2 + n3 + n4) /4
+          print(f'sua média e de {media}:')
+else:
+  
+  media = (n1 + n2 + n3) / 3
+  print('sua media e de {media}:')
+  

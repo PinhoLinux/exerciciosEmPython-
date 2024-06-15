a05bbd66a501f243c827264943a88f89956b9df6
Emerson pinho <the.emerson.araujo@gmail.com>
2024-06-15 12:10:20 -0300

    Atualizar o

---
+++ tuplas.py
@@ -0,0 +1,37 @@
+# lanche = 'Hamburguer', 'suco', 'pizza', 'pudin'
+# print(lanche[1])
+
+# lanche = 'Hamburguer', 'suco', 'pizza', 'pudim'
+# print(lanche[-3])
+
+# lanche = ('Hamburguer', 'suco', 'pizza', 'pudim')
+# print(lanche[1:3])
+
+# lanche = ('Hamburguer', 'suco', 'pizza','pudim')
+# tuplas sao imutáveis
+# lanche[1] = refrigerante 
+# print(lanche[-3:])
+
+#lanche = ('Hamburguer', 'suco', 'pizza', 'pudim', 'batataa frita')
+# print(lanche)
+
+#for cont in range(0, len(lanche)):
+    #print(f'Eu vou comer {lanche[cont]}')
+
+# para nao aparecer "feinho" você pode fazer:
+lanche = ('Hamburguer', 'suco', 'pizza', 'pudim', 'batataa frita')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posicao {pos}')
+print('como pra caramba!')
+
+
+
+
+
+
+
+
+
+
+
+

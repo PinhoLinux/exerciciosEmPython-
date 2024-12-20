a600aabda2588f0b981e154affa3638241a7163c
PinhoLinux <the.emerson.araujo@gmail.com>
2024-12-20 09:03:41 -0300

    mais um exercicios
    adicionando um exercicio que mostra o sucessor e antecessor dos numeros

---
+++ antecessorESucessor.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um numero para ver seu antecessor e sucessor: '))
+antecessor = (numero -1)
+sucessor = (numero + 1)
+print(f'você digitou {numero}, seu ANTECESSOR é {antecessor} e seu SUCESSOR é {sucessor}')

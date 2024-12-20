bd719c7eff0ee158e6f6a2c06d0e2c67503dfe2b
PinhoLinux <the.emerson.araujo@gmail.com>
2024-12-20 09:56:23 -0300

    mais um exercicio
    mostra o dobro o triplo e a raiz quadrada de um numero

---
+++ dobroTriploRaizQuadrada.py
@@ -0,0 +1,7 @@
+import math
+
+numeroDigitado = int(input('Digite um Número: '))
+dobro = (numeroDigitado * 2)
+triplo = (numeroDigitado * 3)
+raizQuadrada = math.sqrt(numeroDigitado)
+print(f'Você digitou o numero {numeroDigitado}.\no dobro é {dobro}\no triplo é {triplo}\nSua raiz quadrada é {raizQuadrada:.2f} ')

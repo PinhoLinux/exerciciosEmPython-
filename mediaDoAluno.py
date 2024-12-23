61797ab7ff8d1cdddad1e611c54998a058d8e9d3
PinhoLinux <the.emerson.araujo@gmail.com>
2024-12-23 11:44:00 -0300

    coverso de medidas
    converte medidas

---
+++ mediaDoAluno.py
@@ -0,0 +1,8 @@
+nomeDoAluno = str(input('Digite o nome do aluno: '))
+primeiraNota = float(input('Digite a primeira nota do aluno: '))
+segundaNota = float(input('Digite a segunda nota do aluno: '))
+terceiraNota = float(input('Digite a segunda nota do aluno: '))
+media = (primeiraNota + segundaNota + terceiraNota) / 3
+print(f'O aluno {nomeDoAluno} obteve a media de {media:.2f}')
+
+
